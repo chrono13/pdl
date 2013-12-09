f9401d9b3e021d070a24a0b920002891c5cbe4ec
kevinblanchard <kevinblanchard@148.60.37.92>
2013-12-09 17:34:14 +0100

    dico dans l'interface course ok

--- pdl/src/ihm/CourseInterface.java
+++ pdl/src/ihm/CourseInterface.java
@@ -67,16 +67,16 @@ public class CourseInterface extends JPanel {
 		menuBar.setBounds(0, 0, 155, 21);
 		desktopPane.add(menuBar);
 
-		JMenu mnFichier = new JMenu("Fichier");
+		JMenu mnFichier = new JMenu(Dico.dansLedico("Fichier", Dico.langue));
 		menuBar.add(mnFichier);
 
-		JMenuItem mntmRevenir = new JMenuItem("Revenir");
+		JMenuItem mntmRevenir = new JMenuItem(Dico.dansLedico("Retour", Dico.langue));
 		mnFichier.add(mntmRevenir);
 
-		JMenuItem mntmSauvegarder = new JMenuItem("Sauvegarder");
+		JMenuItem mntmSauvegarder = new JMenuItem(Dico.dansLedico("Sauvegarder", Dico.langue));
 		mnFichier.add(mntmSauvegarder);
 
-		JMenuItem mntmQuitter = new JMenuItem("Quitter");
+		JMenuItem mntmQuitter = new JMenuItem(Dico.dansLedico("Quitter", Dico.langue));
 		mntmQuitter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				System.exit(1);
@@ -84,16 +84,16 @@ public class CourseInterface extends JPanel {
 		});
 		mnFichier.add(mntmQuitter);
 
-		JMenu mnEdition = new JMenu("Edition");
+		JMenu mnEdition = new JMenu(Dico.dansLedico("Edition", Dico.langue));
 		menuBar.add(mnEdition);
 
-		JMenuItem mntmRien = new JMenuItem("rien");
+		JMenuItem mntmRien = new JMenuItem(Dico.dansLedico("rien", Dico.langue));
 		mnEdition.add(mntmRien);
 
-		JMenu mnAction = new JMenu("Action");
+		JMenu mnAction = new JMenu(Dico.dansLedico("Action", Dico.langue));
 		menuBar.add(mnAction);
 
-		JMenuItem mntmClassement = new JMenuItem("Classement");
+		JMenuItem mntmClassement = new JMenuItem(Dico.dansLedico("Classement", Dico.langue));
 		mnAction.add(mntmClassement);
 
 		
@@ -127,7 +127,7 @@ public class CourseInterface extends JPanel {
 				panel.add(desktopPane_1);
 
 				JDesktopPane desktopPane_2 = new JDesktopPane();
-				desktopPane_2.setBorder(new TitledBorder(null, "Temps", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+				desktopPane_2.setBorder(new TitledBorder(null, Dico.dansLedico("Temps", Dico.langue), TitledBorder.LEADING, TitledBorder.TOP, null, null));
 				desktopPane_2.setBackground(new Color(230, 230, 250));
 				desktopPane_2.setBounds(10, 25, 290, 220);
 				desktopPane_1.add(desktopPane_2);
@@ -140,12 +140,12 @@ public class CourseInterface extends JPanel {
 				desktopPane_2.add(progressBar);
 
 
-				JLabel lblHeureActuelle = new JLabel("Heure actuelle :");
+				JLabel lblHeureActuelle = new JLabel(Dico.dansLedico("Heure actuelle :", Dico.langue));
 				lblHeureActuelle.setFont(new Font("Segoe UI", Font.BOLD, 12));
 				lblHeureActuelle.setBounds(10, 27, 102, 14);
 				desktopPane_2.add(lblHeureActuelle);
 		
-				JLabel lblHeureDeFin = new JLabel("Heure de fin pr\u00E9vu :");
+				JLabel lblHeureDeFin = new JLabel(Dico.dansLedico("Heure de fin prevue :", Dico.langue));
 				lblHeureDeFin.setFont(new Font("Segoe UI", Font.BOLD, 12));
 				lblHeureDeFin.setBounds(10, 60, 124, 14);
 				desktopPane_2.add(lblHeureDeFin);
@@ -166,34 +166,34 @@ public class CourseInterface extends JPanel {
 				heurefin.setColumns(10);
 				heurefin.setText(session.getSession_heurefin());
 				
-				JLabel lblTempscoul = new JLabel("Temps \u00E9coul\u00E9 :");
+				JLabel lblTempscoul = new JLabel(Dico.dansLedico("Temps ecoule :", Dico.langue));
 				lblTempscoul.setFont(new Font("Segoe UI", Font.BOLD, 12));
 				lblTempscoul.setBounds(10, 92, 124, 14);
 				desktopPane_2.add(lblTempscoul);
 				
 
 				JDesktopPane desktopPane_3 = new JDesktopPane();
-				desktopPane_3.setBorder(new TitledBorder(null, "Information pour le pilote", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+				desktopPane_3.setBorder(new TitledBorder(null, Dico.dansLedico("Informations sur le pilote", Dico.langue), TitledBorder.LEADING, TitledBorder.TOP, null, null));
 				desktopPane_3.setBackground(new Color(230, 230, 250));
 				desktopPane_3.setBounds(312, 25, 290, 220);
 				desktopPane_1.add(desktopPane_3);
 
-				JLabel lblPanneauDeLa = new JLabel("Panneau de la voiture :");
+				JLabel lblPanneauDeLa = new JLabel(Dico.dansLedico("Panneau de la voiture :", Dico.langue));
 				lblPanneauDeLa.setFont(new Font("Segoe UI", Font.BOLD, 12));
 				lblPanneauDeLa.setBounds(10, 39, 146, 16);
 				desktopPane_3.add(lblPanneauDeLa);
 
-				JLabel lblToursRestant = new JLabel("Tours Restant :");
+				JLabel lblToursRestant = new JLabel(Dico.dansLedico("Nombre de tour(s) restant(s) :", Dico.langue));
 				lblToursRestant.setFont(new Font("Segoe UI", Font.BOLD, 12));
 				lblToursRestant.setBounds(10, 84, 134, 14);
 				desktopPane_3.add(lblToursRestant);
 
-				JLabel lblTemps = new JLabel("Temps :");
+				JLabel lblTemps = new JLabel(Dico.dansLedico("Temps :", Dico.langue));
 				lblTemps.setFont(new Font("Segoe UI", Font.BOLD, 12));
 				lblTemps.setBounds(10, 125, 118, 14);
 				desktopPane_3.add(lblTemps);
 
-				JLabel lblEtat = new JLabel("Etat :");
+				JLabel lblEtat = new JLabel(Dico.dansLedico("Etat :", Dico.langue));
 				lblEtat.setFont(new Font("Segoe UI", Font.BOLD, 12));
 				lblEtat.setBounds(10, 165, 78, 14);
 				desktopPane_3.add(lblEtat);
@@ -221,22 +221,22 @@ public class CourseInterface extends JPanel {
 				desktopPane_3.add(textPane_6);
 
 				JDesktopPane desktopPane_4 = new JDesktopPane();
-				desktopPane_4.setBorder(new TitledBorder(null, "Informations g\u00E9n\u00E9rales", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+				desktopPane_4.setBorder(new TitledBorder(null, Dico.dansLedico("Informations generales", Dico.langue), TitledBorder.LEADING, TitledBorder.TOP, null, null));
 				desktopPane_4.setBackground(new Color(230, 230, 250));
 				desktopPane_4.setBounds(612, 25, 290, 220);
 				desktopPane_1.add(desktopPane_4);
 
-				JLabel lblPiloteActuel = new JLabel("Pilote actuel :");
+				JLabel lblPiloteActuel = new JLabel(Dico.dansLedico("Pilote actuel :", Dico.langue));
 				lblPiloteActuel.setFont(new Font("Segoe UI", Font.BOLD, 12));
 				lblPiloteActuel.setBounds(10, 56, 95, 14);
 				desktopPane_4.add(lblPiloteActuel);
 
-				JLabel lblNewLabel = new JLabel("Nombre de tours par relais :");
+				JLabel lblNewLabel = new JLabel(Dico.dansLedico("Nombre de tour(s) par relai :", Dico.langue));
 				lblNewLabel.setFont(new Font("Segoe UI", Font.BOLD, 12));
 				lblNewLabel.setBounds(10, 97, 168, 14);
 				desktopPane_4.add(lblNewLabel);
 
-				JLabel lblNombreDeTours = new JLabel("Nombre de tours restant :");
+				JLabel lblNombreDeTours = new JLabel(Dico.dansLedico("Nombre de tour(s) restant(s) :", Dico.langue));
 				lblNombreDeTours.setFont(new Font("Segoe UI", Font.BOLD, 12));
 				lblNombreDeTours.setBounds(10, 136, 157, 14);
 				desktopPane_4.add(lblNombreDeTours);
@@ -294,13 +294,13 @@ public class CourseInterface extends JPanel {
 						return false ; 
 					}
 				};
-				model.addColumn("Voiture");
-				model.addColumn("Tour");
-				model.addColumn("Pilote");
-				model.addColumn("Temps");
-				model.addColumn("Etat");
-				model.addColumn("Heure");
-				model.addColumn("Commentaire");
+				model.addColumn(Dico.dansLedico("Voiture(s)", Dico.langue));
+				model.addColumn(Dico.dansLedico("Tour", Dico.langue));
+				model.addColumn(Dico.dansLedico("Pilote (s)", Dico.langue));
+				model.addColumn(Dico.dansLedico("Temps", Dico.langue));
+				model.addColumn(Dico.dansLedico("Etat", Dico.langue));
+				model.addColumn(Dico.dansLedico("Heure", Dico.langue));
+				model.addColumn(Dico.dansLedico("Commentaires", Dico.langue));
 				
 				
 				
@@ -359,7 +359,7 @@ public class CourseInterface extends JPanel {
 		
 		
 		
-		JButton btnTopDepartPour = new JButton("Top depart pour toutes les voitures");
+		JButton btnTopDepartPour = new JButton(Dico.dansLedico("Top depart pour toutes les voitures", Dico.langue));
 		btnTopDepartPour.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// lance tout les tops des voitures
